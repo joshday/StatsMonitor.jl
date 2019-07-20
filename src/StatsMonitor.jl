31cf4c99fa87840862e883c06c253f78eccddd8f
Josh Day <emailjoshday@gmail.com>
2019-07-20 11:31:13 -0400

    Files generated by PkgTemplates

---
+++ src/StatsMonitor.jl
@@ -0,0 +1,5 @@
+module StatsMonitor
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StatsMonitor
+using Test
+
+@testset "StatsMonitor.jl" begin
+    # Write your own tests here.
+end
