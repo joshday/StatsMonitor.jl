40ed2fc37f3e42176f584363e00b21c549d603cf
Josh Day <emailjoshday@gmail.com>
2019-07-25 14:19:04 -0400

    first commit

--- src/StatsMonitor.jl
+++ src/StatsMonitor.jl
@@ -1,5 +1,53 @@
 module StatsMonitor
 
-greet() = print("Hello World!")
+using Sockets
+using OnlineStats
+using JSON3
+using Dates
+
+#-----------------------------------------------------------------------# init_bucket
+init_bucket() = Dict{Symbol, Vector{Float64}}()
+
+#-----------------------------------------------------------------------# handle_message!
+function handle_message!(bucket, msg)
+    for (k, v) in pairs(JSON3.read(msg))
+        haskey(bucket, k) ? push!(bucket[k], v.val) : (bucket[k] = [v.val])
+    end
+end
+
+#-----------------------------------------------------------------------# Backend
+abstract type StatsBackend end
+
+struct Info <: StatsBackend end
+send(out, backend::Info) = @info "Bucket:" time=now() value=out
+
+#-----------------------------------------------------------------------# Config
+struct Config{B <: StatsBackend}
+    port::Int
+    bucket::Dict{Symbol, Vector{Float64}}
+    backend::B
+    interval::Int
+end
+function Config(;port=8125, bucket=init_bucket(), backend=Info(), interval=1)
+    Config(port, bucket, backend, interval)
+end
+
+function flush!(c::Config)
+    out = Dict(k => (mean(v), var(v), length(v)) for (k, v) in pairs(c.bucket))
+    map(empty!, values(c.bucket))
+    send(out, c.backend)
+end
+
+#-----------------------------------------------------------------------# UDP
+function udp_server(opts::Config = Config())
+    sock = UDPSocket()
+    bind(sock, ip"127.0.0.1", opts.port)
+    timer = Timer(t -> flush!(opts), 1, interval=opts.interval)
+    while true
+        handle_message!(opts.bucket, recv(sock))
+    end
+    close(timer)
+    close(sock)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using StatsMonitor
 using Test
+using Sockets
 
 @testset "StatsMonitor.jl" begin
-    # Write your own tests here.
+
+bucket = Dict{Symbol,Vector{Float64}}()
+
+opts = StatsMonitor.Config(bucket=bucket)
+@async StatsMonitor.udp_server(opts)
+sock = UDPSocket()
+
+function test()
+    t = Timer(t -> send(sock, ip"127.0.0.1", opts.port, """{ "myId": { "val": $(randn()), "stat": "m" } }"""), 1, interval=1/100)
+    t2 = Timer(t -> send(sock, ip"127.0.0.1", opts.port, """{ "myId2": { "val": $(randn()), "stat": "m" } }"""), .9, interval=1/100)
+    sleep(10)
+    close(t)
+    close(t2)
+    close(sock)
+end
+
+test()
+@info bucket
 end
