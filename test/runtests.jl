828dbda130d39d1486a4e75bd02f2bb292647c45
joshday <emailjoshday@gmail.com>
2020-05-14 23:59:20 -0400

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,11 @@ s = Stats(Second(1))
 @showprogress for i in 1:2000
     sleep(.01)
     fit!(s, :id1 => randn() + i/1000)
-    fit!(s, :id1_2 => i * randn())
-    fit!(s, :id2 => rand(["A","A","B","C"]))
-    fit!(s, :id3 => rand() < .8)
+    fit!(s, :id2 => i * randn())
+    fit!(s, :id3 => rand(["A","A","B","C"]))
+
+    # different data types, same id
+    fit!(s, :id4 => rand() < .8)
     rand() < i/2000 && fit!(s, :id4 => nothing)
 end
 
